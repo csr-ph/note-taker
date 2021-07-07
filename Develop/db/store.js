5d32b1d729ef721f5b02b13cdf61e95908b765b9
csr-ph <lucaw10@hotmail.com>
2021-07-07 13:39:55 -0500

    add comments and continue route creation

---
+++ Develop/db/store.js
@@ -0,0 +1,18 @@
+const util = require('util');
+const fs = require('fs');
+
+// npm package for creating ids
+const rndid = require('rndid');
+
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+class Store {
+    read() {
+        return readFile('db/db.json');
+    }
+
+    write(note) {
+        
+    }
+}
--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,17 @@
+// set up dependencies and requirements
 const express = require('express');
 const app = express();
 const { notes } = require('./db/db');
 const path = require('path');
+const fs = require('fs');
 
+// create a blank array to store notes
+let noteData = [];
+
+// set up middleware for app usage
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
+//set up listener for server
 app.listen(3001, () => {
     console.log('Now listening on port 3001');
 });
